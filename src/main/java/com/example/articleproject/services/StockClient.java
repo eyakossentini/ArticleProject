0d0168e9a4aafc3db92d2c13ab2192cd3d5cabe6
Eya Kossentini <eya.kossentini@esprit.tn>
2024-06-22 16:36:30 +0200

    first commit

--- src/main/java/com/example/articleproject/services/StockClient.java
+++ src/main/java/com/example/articleproject/services/StockClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient( name = "stock" , url = "http://localhost:8086")
+@FeignClient( name = "stock" , url = "http://localhost:8085")
 public interface StockClient {
 
     @GetMapping("/stock/getbyid/{id}")
